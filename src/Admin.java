059a2e3e729687545dcba6bcbcadafde1e14987a
zjm <zjmwlx@126.com>
2018-07-05 21:53:39 +0800

    完成添加学生操作，存在BUG！

---
+++ src/Admin.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Admin {//用来管理学生
+	Scanner scanner = new Scanner(System.in);
+	/**添加学生*/
+	public void  AddStudent(String name,int age,Student[] students) {
+		/**创建要添加的学生对象*/
+		Student student = new Student();
+		student.setName(name);
+		student.setAge(age);
+		int i = this.setIndex(students);
+		/**判断是否添加完所有的学生，如果没有则继续添加*/
+		if(i == -1){
+			System.out.println(students.length+"名学生已全部添加完毕！");
+		}else{
+			students[i]  =student;
+		}
+		for(int j = 0;j<students.length;j++){
+		
+			System.out.println(students[i]);
+		}
+		}
+	
+	/**判断添加到里第几个学生*/
+	private int setIndex(Student[] students) {
+		for(int i=0;i<students.length;i++){
+			if (students[i]==null) {
+				return i;
+			}
+		}
+		return -1;
+		
+	}
+
+}
--- src/Text.java
+++ src/Text.java
@@ -1,27 +1,37 @@
 import java.util.Scanner;
 
-public class Text {
+public class Text {//主程序
 
 	public static void main(String[] args) {
 		/** 声明Scanner */
 		Scanner scanner = new Scanner(System.in);
-		
+		/**声明学生管理器*/
+		Admin admin = new Admin();
 		/** 选择要执行的操作的编号 */
 		int num;
 
+		/**程序开始的提示信息*/
 		System.out.println("***请输入学生人数***");
 		/** total:学生总数 */
 		int total = scanner.nextInt();
+		/**保存学生信息的数组*/
 		Student[] students = new Student[total];
+		/**打印提示信息，让用户选择要执行的操作*/
 		System.out.println("***你要执行的操作***\n" + ">10:添加一个学生\n" + ">11:查找以学生\n" + ">12:根据编号更新学生的基本信息\n" + ">13:根据编号删除学生\n"
 				+ ">14:根据编号输入学生的各门成绩\n" + ">15:根据某门成绩进行排序\n" + ">16:根据总分进行排序\n" + ">99:退出系统");
 		num = scanner.nextInt();
-		if (num !=10&&num !=11&&num !=12&&num !=13&&num != 14&&num != 15&&num != 16&&num != 99) {
+		/**判断用户输入是否有误*/
+		if ((num !=10)&&(num !=11)&&(num !=12)&&(num !=13)&&(num != 14)&&(num != 15)&&(num != 16)&&(num != 99)) {
 			System.out.println("请输入正确的操作编号！");
 		}
+		
 		switch (num) {
 		case 10:
-			System.out.println("添加学生");
+			System.out.println("请输入要添加的学生的姓名：");
+			String name = scanner.next();
+			System.out.println("请输入要添加的学生的年龄：");
+			int age = scanner.nextInt();
+			admin.AddStudent(name, age, students);
 			break;
 		case 11:
 			System.out.println("查找学生");
